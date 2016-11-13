bf441017a570e4432d1e43b205dfcf81869f6eea
barrybecker4 <barrybecker4@yahoo.com>
2016-11-13 06:37:27 -0800

    Make code use scala classes and idioms, not java's. Refactor out unit test. Use scala-test and scalatest gradle plugin.

--- src/main/scala/com/barrybecker4/matching/SMP.scala
+++ src/main/scala/com/barrybecker4/matching/SMP.scala
@@ -1,64 +1,48 @@
 package com.barrybecker4.matching
 
+import scala.collection.immutable.TreeMap
+import scala.collection.mutable
+import scala.collection.mutable.ListBuffer
+//import java.util._
+//import scala.collection.JavaConversions._
 
-import java.util._
-import scala.collection.JavaConversions._
+class SMP {
 
-object SMP extends App {
-  def run() {
-    Seq("abe" -> Array("abi", "eve", "cath", "ivy", "jan", "dee", "fay", "bea", "hope", "gay"),
-      "bob" -> Array("cath", "hope", "abi", "dee", "eve", "fay", "bea", "jan", "ivy", "gay"),
-      "col" -> Array("hope", "eve", "abi", "dee", "bea", "fay", "ivy", "gay", "cath", "jan"),
-      "dan" -> Array("ivy", "fay", "dee", "gay", "hope", "eve", "jan", "bea", "cath", "abi"),
-      "ed" -> Array("jan", "dee", "bea", "cath", "fay", "eve", "abi", "ivy", "hope", "gay"),
-      "fred" -> Array("bea", "abi", "dee", "gay", "eve", "ivy", "cath", "jan", "hope", "fay"),
-      "gav" -> Array("gay", "eve", "ivy", "bea", "cath", "abi", "dee", "hope", "jan", "fay"),
-      "hal" -> Array("abi", "eve", "hope", "fay", "ivy", "cath", "jan", "bea", "gay", "dee"),
-      "ian" -> Array("hope", "cath", "dee", "gay", "bea", "abi", "fay", "ivy", "jan", "eve"),
-      "jon" -> Array("abi", "fay", "jan", "gay", "eve", "bea", "dee", "cath", "ivy", "hope"))
-      .foreach { e => guyPrefers.put(e._1, e._2.toList) }
+  def run(guys: List[String], girls: List[String],
+          guyPrefers: Map[String, List[String]], girlPrefers: Map[String, List[String]]): Boolean = {
 
-    Seq("abi" -> Array("bob", "fred", "jon", "gav", "ian", "abe", "dan", "ed", "col", "hal"),
-      "bea" -> Array("bob", "abe", "col", "fred", "gav", "dan", "ian", "ed", "jon", "hal"),
-      "cath" -> Array("fred", "bob", "ed", "gav", "hal", "col", "ian", "abe", "dan", "jon"),
-      "dee" -> Array("fred", "jon", "col", "abe", "ian", "hal", "gav", "dan", "bob", "ed"),
-      "eve" -> Array("jon", "hal", "fred", "dan", "abe", "gav", "col", "ed", "ian", "bob"),
-      "fay" -> Array("bob", "abe", "ed", "ian", "jon", "dan", "fred", "gav", "col", "hal"),
-      "gay" -> Array("jon", "gav", "hal", "fred", "bob", "abe", "col", "ed", "dan", "ian"),
-      "hope" -> Array("gav", "jon", "bob", "abe", "ian", "dan", "hal", "ed", "col", "fred"),
-      "ivy" -> Array("ian", "col", "hal", "gav", "fred", "bob", "abe", "ed", "jon", "dan"),
-      "jan" -> Array("ed", "hal", "gav", "abe", "bob", "jon", "col", "ian", "fred", "dan"))
-      .foreach { e => girlPrefers.put(e._1, e._2.toList) }
-
-    val matches = matching(guys, guyPrefers, girlPrefers)
+    var matches = matching(guys, guyPrefers, girlPrefers)
     matches.foreach { e => println(s"${e._1} is engaged to ${e._2}") }
     if (checkMatches(guys, girls, matches, guyPrefers, girlPrefers))
       println("Marriages are stable")
     else
       println("Marriages are unstable")
 
-    val tmp = matches(girls(0))
-    matches += girls(0) -> matches(girls(1))
+    val tmp = matches(girls.head)
+    matches += girls.head -> matches(girls(1))
     matches += girls(1) -> tmp
-    println(girls(0) + " and " + girls(1) + " have switched partners")
+    println(girls.head + " and " + girls(1) + " have switched partners")
     if (checkMatches(guys, girls, matches, guyPrefers, girlPrefers))
       println("Marriages are stable")
     else
       println("Marriages are unstable")
+    true
   }
 
   private def matching(guys: Iterable[String],
                        guyPrefers: Map[String, List[String]],
                        girlPrefers: Map[String, List[String]]): Map[String, String] = {
-    val engagements = new TreeMap[String, String]
-    val freeGuys = new LinkedList[String](guys)
-    while (!freeGuys.isEmpty) {
-      val guy = freeGuys.remove(0)
+    var engagements = new TreeMap[String, String]
+    var freeGuys = guys.toList
+
+    while (freeGuys.nonEmpty) {
+      val guy = freeGuys.head
+      freeGuys = freeGuys.tail
       val guy_p = guyPrefers(guy)
       var break = false
       for (girl <- guy_p)
         if (!break)
-          if (!engagements.containsKey(girl)) {
+          if (!engagements.contains(girl)) {
             engagements += girl -> guy
             break = true
           }
@@ -67,7 +51,7 @@ object SMP extends App {
             val girl_p = girlPrefers(girl)
             if (girl_p.indexOf(guy) < girl_p.indexOf(other_guy)) {
               engagements += girl -> guy
-              freeGuys += other_guy
+              freeGuys +:= other_guy
               break = true
             }
           }
@@ -80,19 +64,23 @@ object SMP extends App {
                            matches: Map[String, String],
                            guyPrefers: Map[String, List[String]],
                            girlPrefers: Map[String, List[String]]): Boolean = {
-    if (!matches.keySet.containsAll(girls) || !matches.values.containsAll(guys))
+    //if (!matches.keySet.containsAll(girls) || !matches.values.containsAll(guys))
+    if (!(girls.forall(matches.contains) && guys.forall(matches.values.toSet.contains)))
       return false
 
-    val invertedMatches = new TreeMap[String, String]
-    matches.foreach { invertedMatches += _.swap }
+    var invertedMatches = new TreeMap[String, String]
+    matches.foreach {
+      invertedMatches += _.swap
+    }
 
     for ((k, v) <- matches) {
       val shePrefers = girlPrefers(k)
-      val sheLikesBetter = new LinkedList[String]
-      sheLikesBetter.addAll(shePrefers.subList(0, shePrefers.indexOf(v)))
+      var sheLikesBetter: List[String] = List[String]()
+      //val foo: List[String] = shePrefers.slice(0, shePrefers.indexOf(v))
+      sheLikesBetter ++= shePrefers.slice(0, shePrefers.indexOf(v))
       val hePrefers = guyPrefers(v)
-      val heLikesBetter = new LinkedList[String]
-      heLikesBetter.addAll(hePrefers.subList(0, hePrefers.indexOf(k)))
+      var heLikesBetter = List[String]()
+      heLikesBetter ++= hePrefers.slice(0, hePrefers.indexOf(k))
 
       for (guy <- sheLikesBetter) {
         val fiance = invertedMatches(guy)
@@ -115,10 +103,4 @@ object SMP extends App {
     true
   }
 
-  private val guys = "abe" :: "bob" :: "col" :: "dan" :: "ed" :: "fred" :: "gav" :: "hal" :: "ian" :: "jon" :: Nil
-  private val girls = "abi" :: "bea" :: "cath" :: "dee" :: "eve" :: "fay" :: "gay" :: "hope" :: "ivy" :: "jan" :: Nil
-  private val guyPrefers = new HashMap[String, List[String]]
-  private val girlPrefers = new HashMap[String, List[String]]
-
-  run()
 }
--- src/test/scala/com/barrybecker4/matching/SMPSuite.scala
+++ src/test/scala/com/barrybecker4/matching/SMPSuite.scala
@@ -9,8 +9,44 @@ class SMPSuite extends FunSuite {
 
   test("test stable marriage") {
 
-    SMP.run()
-    assert(true)
+
+    val guys: List[String] = "abe" :: "bob" :: "col" :: "dan" :: "ed" :: "fred" :: "gav" :: "hal" :: "ian" :: "jon" :: Nil
+    val girls: List[String] = "abi" :: "bea" :: "cath" :: "dee" :: "eve" :: "fay" :: "gay" :: "hope" :: "ivy" :: "jan" :: Nil
+
+    val guyPrefers: Map[String, List[String]] = Map(
+      "abe" -> List("abi", "eve", "cath", "ivy", "jan", "dee", "fay", "bea", "hope", "gay"),
+      "bob" -> List("cath", "hope", "abi", "dee", "eve", "fay", "bea", "jan", "ivy", "gay"),
+      "col" -> List("hope", "eve", "abi", "dee", "bea", "fay", "ivy", "gay", "cath", "jan"),
+      "dan" -> List("ivy", "fay", "dee", "gay", "hope", "eve", "jan", "bea", "cath", "abi"),
+      "ed" -> List("jan", "dee", "bea", "cath", "fay", "eve", "abi", "ivy", "hope", "gay"),
+      "fred" -> List("bea", "abi", "dee", "gay", "eve", "ivy", "cath", "jan", "hope", "fay"),
+      "gav" -> List("gay", "eve", "ivy", "bea", "cath", "abi", "dee", "hope", "jan", "fay"),
+      "hal" -> List("abi", "eve", "hope", "fay", "ivy", "cath", "jan", "bea", "gay", "dee"),
+      "ian" -> List("hope", "cath", "dee", "gay", "bea", "abi", "fay", "ivy", "jan", "eve"),
+      "jon" -> List("abi", "fay", "jan", "gay", "eve", "bea", "dee", "cath", "ivy", "hope"))
+
+
+    val girlPrefers: Map[String, List[String]] = Map(
+      "abi" -> List("bob", "fred", "jon", "gav", "ian", "abe", "dan", "ed", "col", "hal"),
+      "bea" -> List("bob", "abe", "col", "fred", "gav", "dan", "ian", "ed", "jon", "hal"),
+      "cath" -> List("fred", "bob", "ed", "gav", "hal", "col", "ian", "abe", "dan", "jon"),
+      "dee" -> List("fred", "jon", "col", "abe", "ian", "hal", "gav", "dan", "bob", "ed"),
+      "eve" -> List("jon", "hal", "fred", "dan", "abe", "gav", "col", "ed", "ian", "bob"),
+      "fay" -> List("bob", "abe", "ed", "ian", "jon", "dan", "fred", "gav", "col", "hal"),
+      "gay" -> List("jon", "gav", "hal", "fred", "bob", "abe", "col", "ed", "dan", "ian"),
+      "hope" -> List("gav", "jon", "bob", "abe", "ian", "dan", "hal", "ed", "col", "fred"),
+      "ivy" -> List("ian", "col", "hal", "gav", "fred", "bob", "abe", "ed", "jon", "dan"),
+      "jan" -> List("ed", "hal", "gav", "abe", "bob", "jon", "col", "ian", "fred", "dan"))
+
+
+    val smp: SMP = new SMP()
+
+    assert(smp.run(guys, girls, guyPrefers, girlPrefers))
   }
+
+  /*
+  test("test stable marriage fail") {
+    assert(false)
+  }*/
 
 }
